835a9a7df80e31130fa40df31899e80ab338a42f
Sujoy Sarker <sujoyict@gmail.com>
2023-03-30 22:48:24 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const calculateBMI = () => {
-  const weight = document.getElementById("weightInput").value;
-  const height = document.getElementById("heightInput").value / 100; // convert cm to m
-  const bmi = weight / (height * height);
-  document.getElementById("result").innerHTML = `Your BMI is: ${bmi.toFixed(2)}`;
+    const weight = document.getElementById("weightInput").value;
+    const height = document.getElementById("heightInput").value / 100; // converting cm to meters
+    const result = document.getElementById("bmiResult");
+
+    if (weight > 0 && height > 0) {
+        const bmi = weight / (height * height);
+        result.innerText = bmi.toFixed(2);
+    } else {
+        result.innerText = "Invalid input";
+    }
 };
 
 document.getElementById("calculateBtn").addEventListener("click", calculateBMI);
